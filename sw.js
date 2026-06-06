096bb9b6d1d68d469a3cdc80871b06e1e11ccb87
Oğuzhan <oguzhanarslan4607@gmail.com>
2026-06-06 13:54:11 +0300

    Add corporate sales UI variant

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-// Uygulamanızın versiyonu. Önbelleğin güncellenmesi için v4.2 yapıldı.
+// Uygulamanızın versiyonu. Önbelleğin güncellenmesi için v4.3 yapıldı.
 // Her yeni HTML/JS/CSS değişikliğinde bu rakamı değiştirin!
-const CACHE_NAME = 'primpro-store-v4.2';
+const CACHE_NAME = 'primpro-store-v4.3';
 
 const ASSETS_TO_CACHE = [
   '/',
