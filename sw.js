e99336dbc7c35f1a35da91cfa4ea70fb24b009c2
Oğuzhan <oguzhanarslan4607@gmail.com>
2026-06-16 12:09:08 +0300

    Add product card picker variant

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-// Uygulamanızın versiyonu. Önbelleğin güncellenmesi için v4.6 yapıldı.
+// Uygulamanızın versiyonu. Önbelleğin güncellenmesi için v4.7 yapıldı.
 // Her yeni HTML/JS/CSS değişikliğinde bu rakamı değiştirin!
-const CACHE_NAME = 'primpro-store-v4.6';
+const CACHE_NAME = 'primpro-store-v4.7';
 
 const ASSETS_TO_CACHE = [
   '/',
