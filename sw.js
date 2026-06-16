a05f6ae950a8b2cc2538290630dc71e10def286e
Oğuzhan <oguzhanarslan4607@gmail.com>
2026-06-16 17:08:15 +0300

    Hesap sonrası sonuç alanına kaydır

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Uygulamanızın versiyonu. Önbelleğin güncellenmesi için v5.1 yapıldı.
 // Her yeni HTML/JS/CSS değişikliğinde bu rakamı değiştirin!
-const CACHE_NAME = 'primpro-store-v5.2';
+const CACHE_NAME = 'primpro-store-v5.4';
 
 const ASSETS_TO_CACHE = [
   '/',
